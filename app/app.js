8bf651d311e632e81edea44b168919e0b49acfc4
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2024-01-11 09:03:16 +0300

    feat: add folder deleted trigger

--- app/app.js
+++ app/app.js
@@ -18,6 +18,7 @@ const {
   fileCreated,
   fileDeleted,
   folderCreated,
+  folderDeleted,
   roomArchived,
   roomCreated
 } = require("./zapier/files/triggers.js")
@@ -36,6 +37,7 @@ const App = {
     [fileCreated.key]: fileCreated,
     [fileDeleted.key]: fileDeleted,
     [folderCreated.key]: folderCreated,
+    [folderDeleted.key]: folderDeleted,
     [roomArchived.key]: roomArchived,
     [roomCreated.key]: roomCreated,
     [userAdded.key]: userAdded
