0aaaf3e202c1f668763c8ae93810fd8c33d551a2
Sergey Linnik <sergey.linnik@onlyoffice.com>
2024-03-21 17:19:02 +0300

    1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-##
+## 1.0.0
 ## Added
 - Basic authorizarion in DocSpace
 - Polling for triggers 
--- app/app.test.js
+++ app/app.test.js
@@ -55,7 +55,7 @@ const { sessionAuth } = require("./zapier/auth/auth.js")
 const { userAdded } = require("./zapier/people/triggers.js")
 
 test("has the actual version", () => {
-  const version = "0.0.2"
+  const version = "1.0.0"
   equal(App.version, version)
   equal(pack.version, version)
 })
